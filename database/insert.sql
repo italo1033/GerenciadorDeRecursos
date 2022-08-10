ca7de34ea68163e40b1a31b226c8b155b543d6d7
italo1033 <italogammy1033@gmail.com>
2022-08-10 03:17:12 -0300

    add inserts final and modification in sql initial

--- database/insert.sql
+++ database/insert.sql
@@ -9,12 +9,12 @@ INSERT INTO resource_file (`name`) VALUES ("Recurso 5");
 INSERT INTO resource_file (`name`) VALUES ("Recurso 6");
 
 
--- inserts resource file 
+-- inserts administrator 
 INSERT INTO administrator (`name`) VALUES ("Vitoria");
 INSERT INTO administrator (`name` ) VALUES ("Italo");
 
 
--- inserts resource file 
+-- inserts administrator_has_resource
 INSERT INTO administrator_has_resource (`administratorID`, `resource_fileID`) VALUES (1,1);
 INSERT INTO administrator_has_resource (`administratorID`, `resource_fileID`) VALUES (1,2);
 INSERT INTO administrator_has_resource (`administratorID`, `resource_fileID`) VALUES (1,3);
@@ -22,13 +22,15 @@ INSERT INTO administrator_has_resource (`administratorID`, `resource_fileID`) VA
 INSERT INTO administrator_has_resource (`administratorID`, `resource_fileID`) VALUES (2,5);
 INSERT INTO administrator_has_resource (`administratorID`, `resource_fileID`) VALUES (2,6);
 
+-- inserts user
+INSERT INTO user (`name`, `administratorID`) VALUES ("Luiz", 1);
+INSERT INTO user (`name`, `administratorID`) VALUES ("monelly", 1);
+INSERT INTO user (`name`, `administratorID`) VALUES ("rayra", 2);
+INSERT INTO user (`name`, `administratorID`) VALUES ("eduarda", 2);
+
+-- inserts alocation
+INSERT INTO alocation (`resource_fileID`, `dateIntial`, `dateFinal`) VALUES (1,"2022-05-10", "2022-05-13")
 
 
-
-
-
--- INSERT INTO user (`name`) VALUES ()
--- INSERT INTO resource_file (`name`) VALUES ()
--- INSERT INTO alacation (`name`) VALUES ()
--- INSERT INTO user_has_Alacation (`name`) VALUES ()
--- INSERT INTO administrator_has_resource (`name`) VALUES ()
+-- inserts user_has_alocation
+INSERT INTO user_has_alocation (`useID`,`alocationID`) VALUES (1,1);
--- database/resourceMenage.sql
+++ database/resourceMenage.sql
@@ -28,19 +28,19 @@ CREATE TABLE administrator_has_resource(
   PRIMARY KEY (idAdministrator_has_resource)
 );
 
-CREATE TABLE  alacation(
-  idAlacation INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE  alocation(
+  idAlocation INT NOT NULL AUTO_INCREMENT,
   resource_fileID INT NOT NULL,
-  dataIntial DATETIME NULL,
-  datafinal DATETIME NULL,
-  PRIMARY KEY (idAlacation)
+  dateIntial DATE NULL,
+  dateFinal DATE NULL,
+  PRIMARY KEY (idAlocation)
 );
 
-CREATE TABLE user_has_Alacation(
-  idUser_has_Alacation INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE user_has_alocation(
+  idUser_has_Alocation INT NOT NULL AUTO_INCREMENT,
   useID INT NOT NULL,
-  AlacationID INT NOT NULL,
-  PRIMARY KEY (idUser_has_Alacation)
+  alocationID INT NOT NULL,
+  PRIMARY KEY (idUser_has_Alocation)
 );
 
 
@@ -52,7 +52,7 @@ CREATE TABLE user_has_Alacation(
 ALTER TABLE user ADD FOREIGN KEY (administratorID) REFERENCES administrator(idAdministrator);
 
 -- add FK USER ALOCATION
-ALTER TABLE alacation ADD FOREIGN KEY (resource_fileID) REFERENCES resource_file(id_resource_file);
+ALTER TABLE alocation ADD FOREIGN KEY (resource_fileID) REFERENCES resource_file(id_resource_file);
 
 -- add FK RESOURCE e ADMINISTRATOR
 ALTER TABLE administrator_has_resource ADD FOREIGN KEY (administratorID) REFERENCES administrator(idAdministrator);
@@ -60,8 +60,8 @@ ALTER TABLE administrator_has_resource ADD FOREIGN KEY (resource_fileID) REFEREN
 
 
 -- add FK RESOURCE e ADMINISTRATOR
-ALTER TABLE user_has_Alacation ADD FOREIGN KEY (useID) REFERENCES user(idUser);
-ALTER TABLE user_has_Alacation ADD FOREIGN KEY (AlacationID) REFERENCES alacation(idAlacation);
+ALTER TABLE user_has_alocation ADD FOREIGN KEY (useID) REFERENCES user(idUser);
+ALTER TABLE user_has_alocation ADD FOREIGN KEY (alocationID) REFERENCES alocation(idAlocation);
 
 
 
