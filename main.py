5df209ca35ed9f96f4b2e7a152c694336a2ebd95
italo1033 <italogammy1033@gmail.com>
2022-08-11 15:15:40 -0300

    add api

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from server.dbController import DbController
+from fastapi import FastAPI
 
 conection = DbController()
-# conection.select()
-# conection.insertResource("Azul")
-# conection.updateResource(19,"verde")
-# conection.deleteResource(19)
-# conection.deleteUser(2)
-conection.AlocationResource(5,3,"2022-05-05", "2022-06-06" )
+# # 
+# # conection.insertResource("Azul")
+# # conection.updateResource(19,"verde")
+# # conection.deleteResource(19)
+# # conection.deleteUser(2)
+# conection.AlocationResource(5,3,"2022-05-05", "2022-06-06" )
+
+
+app = FastAPI()
+
+
+@app.get("/listResource")
+def listResource():
+    return conection.select()
+
+
+@app.post("/insertResource/{name}")
+def addResource(name):
+    return conection.insertResource(name)
