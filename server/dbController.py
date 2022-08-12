1ffee5072fe3e6b7ce86ef2707054c1b82b6196e
italo1033 <italogammy1033@gmail.com>
2022-08-11 23:07:15 -0300

    add try

--- server/dbController.py
+++ server/dbController.py
@@ -43,51 +43,64 @@ class DbController():
                 
 
     def updateResource(self, id, resource):
-        connection = self.getConnection()
-        with connection:
-            with connection.cursor() as cursor:
-                sql = 'UPDATE resource_file  SET name=%s WHERE id_resource_file=%s'
-                cursor.execute(sql,(resource , id))
-                connection.commit()
-                print("Recurso Atualizado!")
+        try:
+            connection = self.getConnection()
+            with connection:
+                with connection.cursor() as cursor:
+                    sql = 'UPDATE resource_file  SET name=%s WHERE id_resource_file=%s'
+                    cursor.execute(sql,(resource , id))
+                    connection.commit()
+                    print("Recurso Atualizado!")
+        except Exception as e: 
+            return {"message": e}
+        
     def deleteResource(self, id):
-        connection = self.getConnection()
-        with connection:
-            with connection.cursor() as cursor:
-                sql = 'DELETE FROM resource_file WHERE id_resource_file=%s'
-                cursor.execute(sql,(id))
-                connection.commit()
-                print("Recurso Deletado!")
-    
+        try:
+            connection = self.getConnection()
+            with connection:
+                with connection.cursor() as cursor:
+                    sql = 'DELETE FROM resource_file WHERE id_resource_file=%s'
+                    cursor.execute(sql,(id))
+                    connection.commit()
+                    print("Recurso Deletado!")
+        except Exception as e: 
+            return {"message": e}
+
     def deleteUser(self, id):
-        connection = self.getConnection()
-        with connection:
-            with connection.cursor() as cursor:
-                sql = """ SELECT idAlocation FROM alocation where userID=%s"""
-                cursor.execute(sql,(id))
-                result = cursor.fetchall()
-                if(result):
-                    query= """DELETE FROM alocation  WHERE userID = %s"""
-                    cursor.execute(query,(id))
-                    connection.commit()
-                    sql = """DELETE FROM user WHERE idUser=%s"""
+        try:
+            connection = self.getConnection()
+            with connection:
+                with connection.cursor() as cursor:
+                    sql = """ SELECT idAlocation FROM alocation where userID=%s"""
                     cursor.execute(sql,(id))
-                    connection.commit()
-                    print("Usuario Deleteado!")
-                else:
-                    sql = """DELETE FROM user WHERE idUser=%s"""
-                    cursor.execute(sql,(id))
-                    connection.commit()
-                    print("Usuario Deleteado!")
+                    result = cursor.fetchall()
+                    if(result):
+                        query= """DELETE FROM alocation  WHERE userID = %s"""
+                        cursor.execute(query,(id))
+                        connection.commit()
+                        sql = """DELETE FROM user WHERE idUser=%s"""
+                        cursor.execute(sql,(id))
+                        connection.commit()
+                        print("Usuario Deleteado!")
+                    else:
+                        sql = """DELETE FROM user WHERE idUser=%s"""
+                        cursor.execute(sql,(id))
+                        connection.commit()
+                        print("Usuario Deleteado!")
+        except Exception as e: 
+            return {"message": e}
 
     def AlocationResource(self, idResource, idUser, dataInitial, dataFinal):
-        connection = self.getConnection()
-        with connection:
-            with connection.cursor() as cursor:
-                sql = 'INSERT INTO alocation (`resource_fileID`,`userID`, `dateIntial`, `dateFinal`) VALUES (%s,%s,%s,%s)'
-                cursor.execute(sql,(idResource, idUser, dataInitial, dataFinal))
-                connection.commit()
-                print("Locação Registrada!")
+        try:
+            connection = self.getConnection()
+            with connection:
+                with connection.cursor() as cursor:
+                    sql = 'INSERT INTO alocation (`resource_fileID`,`userID`, `dateIntial`, `dateFinal`) VALUES (%s,%s,%s,%s)'
+                    cursor.execute(sql,(idResource, idUser, dataInitial, dataFinal))
+                    connection.commit()
+                    print("Locação Registrada!")
+        except Exception as e: 
+            return {"message": e}
 
         
         
